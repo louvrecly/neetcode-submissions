6d7fb925eacd6ba8e43c6154b8c5abeb042e374c
Louvre <47293355+louvrecly@users.noreply.github.com>
2026-09-08 16:38:01 -0400

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        # [2 4 6 8 10] | l: 5
+        # [2 10 4 8 6]
+        # 2 > 4 > 6 > 8 > 10 > N
+        # ^ | [2]
+        #     ^ | [2 4]
+        #         ^ | [2 4 6]
+        #             ^ | [2 4 6 8]
+        #                  ^ | [2 4 6 8 10]
+        # H > N | [2 4 6 8 10]
+        # H > 2 > N | [4 6 8 10]
+        # H > 2 > 10 > N | [4 6 8]
+        # H > 2 > 10 > 4 > N | [6 8]
+        # H > 2 > 10 > 4 > 8 > N | [6]
+        # H > 2 > 10 > 4 > 8 > 6 > N | []
+        # Time: O(n) | Space: O(n)
+        pointer = head
+        nodes = deque([])
+
+        while pointer:
+            nodes.append(pointer)
+            pointer = pointer.next
+
+        n = len(nodes)
+        head = ListNode()
+        pointer = head
+
+        for i in range(n):
+            nextNode = nodes.pop() if i % 2 else nodes.popleft()
+            pointer.next = nextNode
+            pointer = pointer.next
+
+        pointer.next = None
+        head = head.next
