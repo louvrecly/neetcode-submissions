cc267e2dbbd03540bfea05cdeea5e76cc8a2ce47
Louvre <47293355+louvrecly@users.noreply.github.com>
2026-09-08 17:26:55 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,49 @@
+#       *
+#    /  |  \
+#  d    b    m
+#  |    |    |
+#  a    a    a
+#  |    |   / \
+#  y*   y* n*  y*
+#          |
+#          y*
+
+class Node:
+    def __init__(self):
+        self.children = {}  # mapping char -> Node
+        self.isEnd = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.children = {}  # mapping char -> Node
+        self.isEnd = False
+
+    def addWord(self, word: str) -> None:
+        pointer = self
+        for char in word:
+            if char not in pointer.children:
+                pointer.children[char] = Node()
+            pointer = pointer.children[char]
+        pointer.isEnd = True
+
+    def searchSubWord(self, word: str, node: Node) -> bool:
+        n = len(word)
+        if n == 0:
+            return node.isEnd
+
+        char = word[0]
+        if char == '.':
+            for child in node.children.values():
+                if self.searchSubWord(word[1:], child):
+                    return True
+            return False
+
+        if char not in node.children:
+            return False
+
+        nextNode = node.children[char]
+        return self.searchSubWord(word[1:], nextNode)
+
+    def search(self, word: str) -> bool:
+        return self.searchSubWord(word, self)
