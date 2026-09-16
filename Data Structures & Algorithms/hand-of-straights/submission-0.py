2f2fae4c5f0aac20e5728a3931f526f4b06cf343
Louvre <47293355+louvrecly@users.noreply.github.com>
2026-09-15 23:19:15 -0400

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        # [1 2 4 2 3 5 3 4] | s: 4
+        # [1 2 3 4] [2 3 4 5]
+        # [1 2 3 3 4 5 6 7] | s: 4
+        # [1 2 3 4] [3 5 6 7]
+        # [1 2 4 2 3 5 3 4] | s: 4
+        # c: { 1: 1, 2: 2, 3: 2, 4: 2, 5: 1 }
+        # [2 4 1 2 3 5 3 4] | s: 4
+        #  ^ | 2 | 2-1=1 | [] | c: { 1: 1, 2: 2, 3: 2, 4: 2, 5: 1 }
+        #    ^ | 4 | 4-1=3 | [] | c: { 1: 1, 2: 2, 3: 2, 4: 2, 5: 1 }
+        #      ^ | 1 | 1-1=0 | [1] | c: { 1: 0, 2: 2, 3: 2, 4: 2, 5: 1 }
+        #      ^ | 1 | 1+1=2 | [1 2] | c: { 1: 0, 2: 1, 3: 2, 4: 2, 5: 1 }
+        #      ^ | 1 | 2+1=3 | [1 2 3] | c: { 1: 0, 2: 1, 3: 1, 4: 2, 5: 1 }
+        #      ^ | 1 | 3+1=4 | [1 2 3 4] | c: { 1: 0, 2: 1, 3: 1, 4: 1, 5: 1 }
+        #        ^ | 2 | 2-1=1 | [2] | c: { 1: 0, 2: 0, 3: 1, 4: 1, 5: 1 }
+        #        ^ | 2 | 2+1=3 | [2 3] | c: { 1: 0, 2: 0, 3: 0, 4: 1, 5: 1 }
+        #        ^ | 2 | 3+1=4 | [2 3 4] | c: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 1 }
+        #        ^ | 2 | 4+1=5 | [2 3 4 5] | c: { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
+        # Time: O(n log n) | Space: O(n)
+        hand.sort()
+        counter = {}
+        remain = 0
+        for num in hand:
+            counter[num] = counter.get(num, 0) + 1
+            remain += 1
+
+        count = 0
+        while remain:
+            num = hand.pop(0)
+            if counter.get(num, 0):
+                currGroup = 0
+                while counter.get(num, 0) and currGroup < groupSize:
+                    counter[num] -= 1
+                    remain -= 1
+                    currGroup += 1
+                    num += 1
+                if currGroup < groupSize:
+                    return False
+        return True
